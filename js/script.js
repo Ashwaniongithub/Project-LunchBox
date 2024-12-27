067eb7b6c7cfcf06d1a2716026ebebd45c46bf4e
ashwani hada <ashwanihada13@gmail.com>
2024-12-27 17:44:46 +0530

    customer page completed

--- js/script.js
+++ js/script.js
@@ -24,3 +24,65 @@ function colorLink(){
 }
 
 linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+
+// datatable js
+$(document).ready(function () {
+    // Initialize DataTable
+    var table = $('#example').DataTable({
+    //   dom: 'Bfrtip',
+      buttons: [
+        {
+          extend: 'excelHtml5',
+          title: 'Data Export'
+        },
+        {
+          extend: 'pdfHtml5',
+          title: 'Data Export'
+        },
+        {
+          extend: 'print',
+          title: 'Data Export'
+        }
+      ]
+    });
+
+    // Custom Button Handlers
+    $('#btnExcel').on('click', function () {
+      table.button('.buttons-excel').trigger();
+    });
+
+    $('#btnPDF').on('click', function () {
+      table.button('.buttons-pdf').trigger();
+    });
+
+    $('#btnPrint').on('click', function () {
+      table.button('.buttons-print').trigger();
+    });
+  });
+
+
+
+
+
+//   sweet alert js
+function showSuccessAlert() {
+    Swal.fire({
+        title: 'Success!',
+        text: 'Data has been added successfully',
+        icon: 'success',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#3085d6',
+        timer: 2000,
+        timerProgressBar: true,
+        toast: false,
+        position: 'center',
+        showConfirmButton: true
+    }).then((result) => {
+        // You can handle post-alert actions here
+        if (result.isConfirmed) {
+            // Do something when user clicks OK
+            console.log('User clicked OK');
+        }
+    });
+}
